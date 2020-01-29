8121630577bfc67162401fe3760f2246589844c8
juanvalenciaquintero <juan140269@gmail.com>
2020-01-29 15:58:52 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,8 @@ export class AppComponent
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
-    this.initializeApp();
+		this.initializeApp();
+
   }
 
   initializeApp() {
